f2bee10b7a3c3eb83f964b4434587bf01ed64c35
Vybavnag <vybavnag980@gmail.com>
2023-11-06 22:36:42 -0800

    Commit changes

--- app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -1,21 +1,77 @@
 package com.driuft.sharedlifecycle
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var _sharedPreferences: SharedPreferences
+    private fun unregisterListener() {
+        _sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        _sharedPreferences = this.getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
+        _sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+        with (_sharedPreferences.edit()) {
+            // replace "onCreate" with the name of the method you're saving
+            putString(getString(R.string.current_lifecycle_state), "onCreate")
+            apply()
+        }
+
+        // Read the state
+        val currentState = _sharedPreferences.getString(
+            getString(R.string.current_lifecycle_state),
+            resources.getString(R.string.default_state)
+        ) ?: resources.getString(R.string.default_state)
+
+
+
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onCreate")
+        // Toast
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    private val listener =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            Toast.makeText(
+                this,
+                sharedPreferences.getString(
+                    key,
+                    resources.getString(R.string.default_state)
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     override fun onStart() {
         super.onStart()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onStart")
+        // Toast
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onStart")
+        // Toast
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
@@ -32,5 +88,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onStart")
+        // Toast
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+        unregisterListener()
     }
 }
